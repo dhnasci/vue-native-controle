13b51f478181eaf5c6fef0e59a31e2537330fe02
Dirceu Henrique <dhnasci@hotmail.com>
2021-01-11 17:24:52 -0400

    Initial version

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="container">
+    <Navbar />
+    <text class="text-color-primary">Controle Bem App</text>
+  </view>
+</template>
+
+<script>
+import Navbar from './src/Navbar/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ src/Navbar/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="main-navbar">
+      <StatusBar backgroundColor="teal" barStyle="light-content" />
+      <text class="navbar-title">Controle Bem</text>
+  </view>
+</template>
+
+<script>
+import { StatusBar } from 'react-native'
+
+export default {
+    components: {
+        StatusBar
+    }
+}
+</script>
+
+<style>
+.main-navbar {
+    background-color: teal;
+    height: 60px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-end;
+    padding-bottom: 10px;
+}
+
+.navbar-title {
+    color: white;
+    font-weight: bold;
+    font-size: 21px;
+    line-height: 33px;
+    letter-spacing: -0.24;
+    color: #fff;
+    padding-left: 15px;
+}
+
+</style>
