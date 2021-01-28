bf0872e77cbb81da89e652fa194624ae198e4b20
Dirceu Henrique <dhnasci@hotmail.com>
2021-01-28 15:08:48 -0400

    Add Picker from react-native with errors

--- src/Ativos/ControleAtivos.vue
+++ src/Ativos/ControleAtivos.vue
@@ -15,29 +15,51 @@
                 borderBottomWidth: 1,
               }" v-model="centroDeCusto" /> 
           </view>
-          <image :source="require('./../../assets/qr_code_black.png')" /> 
+          <view class="filter-box-row">
+            <image :source="require('./../../assets/qr_code_black.png')" /> 
+           
+            <Picker
+              mode="dropdown"
+              ref="myPicker"
+              :onValueChange="callStatus"
+              :selectedValue="selecionado"
+              :style="{ fontSize: 16, lineHeight: 16, width: 234, borderBottomWidth: 1 }" >
+
+              <Picker.Item label="ATIVO" value="ativo" />
+              <Picker.Item label="EM MANUTENÇÃO" value="manutencao" />
+              <Picker.Item label="BAIXADO" value="baixado" />
+              
+            </Picker>
+          
+          </view>
         </view>
         <view class="drawer-text">
           <text style="font-family: Roboto; font-style: normal; font-weight: bold; font-size: 18; line-height: 21; ">Controle de Ativos </text>
           <image :source="require('./../../assets/add_box.png')" /> 
+          
         </view>
       </view>
     </view>
 </template>
 
 <script>
+
 import Navbar from './../Navbar.vue'
 import * as Font from 'expo-font';
+import  { Picker } from '@react-native-picker/picker';
+
 
 export default {
   data() {
     return {
       isAppReady: false,
       centroDeCusto: '',
+      selecionado: 'ativo',
     };
   },
   components: {
-    Navbar
+    Navbar, 
+    Picker,
   },
   props: { 
     navigation: {
@@ -47,10 +69,19 @@ export default {
   created() {
     this.loadFonts();
   },
+  mounted() {
+    //console.log('vue-controle-bem mounted > ', this.$refs.myPicker);
+    console.log('vue-controle-bem mounted > ');
+
+  },
   methods: {
     handleMenu() {
       console.log('vue-controle-bem Controle Ativos handleMenu');
       this.navigation.openDrawer();
+    },
+    callStatus (value, index) {
+      console.log('vue-controle-bem Controle Ativo Status ', value);
+      console.log('vue-controle-bem Controle Ativo index ', index);
     },
     async loadFonts() {
       try {
@@ -90,6 +121,11 @@ export default {
   align-items: center;
   justify-content: flex-start;
   padding-bottom: 16px;
+}
+.centraliza {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 </style>
